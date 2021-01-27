f28e7a7c0055b7d9307bd1ab190525a0070289b0
Pablo Albaladejo <pablo.albaladejo@guidesmiths.com>
2021-01-27 19:04:16 +0100

    chore(master): build

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const ExampleComponent = ({ text }) => {
+  return (
+    <div
+      style={{
+        fontWeight: "bold",
+        fontSize: 4,
+        color: "primary",
+      }}
+    >
+      Example Component: {text}
+    </div>
+  );
+};
